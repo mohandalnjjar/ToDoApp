b3978494afd2d77ec12fd131b5a8fd33c3d7679b
mohand <mohanadalnjjar23@gmail.com>
2024-03-15 10:39:26 +0200

    ceate type adapter for to do model

--- lib/features/to_do_feature/data/models/to_do_model.dart
+++ lib/features/to_do_feature/data/models/to_do_model.dart
@@ -1,5 +1,16 @@
-class ToDoModel {
+import 'package:hive_flutter/adapters.dart';
+
+
+
+part 'to_do_model.g.dart';
+
+
+@HiveType(typeId: 0)
+class ToDoModel extends HiveObject {
+  @HiveField(0)
   final String title;
+
+  @HiveField(1)
   bool isDone;
 
   ToDoModel({this.isDone = false, required this.title});
